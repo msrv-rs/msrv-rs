60a85e44893984362d4f4b8b5b4c95a91207f69c
est31 <MTest31@outlook.com>
2020-03-07 11:02:28 +0100

    Use Box<_> in the FFI
    The ABI of Box<T> is documented to be the same
    as the ABI of * mut T.
    
    https://doc.rust-lang.org/std/boxed/index.html#memory-layout
    
    Thanks to @s3bk for the pointer.

--- src/lib.rs
+++ src/lib.rs
@@ -16,23 +16,21 @@ pub extern fn plugin_query(ctx: &mut Plugin, s: * const u8, len: usize) -> u32 {
 }
 
 #[no_mangle]
-pub extern fn plugin_init(s: * const u8, len: usize) -> * mut Plugin {
+pub extern fn plugin_init(s: * const u8, len: usize) -> Box<Plugin> {
     let params_str = unsafe {
         let sl = std::slice::from_raw_parts(s, len);
         std::str::from_utf8(sl)
     };
     let excludes = get_excludes(params_str.ok())
         .unwrap_or_else(|| Vec::new());
-    let ctx = Box::new(Plugin::new(excludes));
-    Box::into_raw(ctx)
+    Box::new(Plugin::new(excludes))
 }
 
 #[no_mangle]
-pub extern fn plugin_delete(ctx: * mut Plugin) {
-    // Obtain the Box and drop it
-    let _b = unsafe {
-        Box::from_raw(ctx)
-    };
+pub extern fn plugin_delete(ctx: Box<Plugin>) {
+    // The call isn't needed but it makes the intent
+    // of dropping explicit
+    std::mem::drop(ctx);
 }
 
 fn get_excludes(params_str: Option<&str>) -> Option<Array> {
